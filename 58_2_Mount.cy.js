c90c32e6d49882c69161e1e3612d7476712874c7
shahidwaris <71723136+shahidwaris@users.noreply.github.com>
2023-03-12 07:47:11 +0530

    Update 58_2_Mount.cy.js

--- 58_2_Mount.cy.js
+++ 58_2_Mount.cy.js
@@ -3,6 +3,12 @@ describe("MyComponent", () => {
         cy.visit('http://localhost:5173/');
       });
 
+    it('should fetch data from API when mounted', () => {
+        cy.intercept('https://jsonplaceholder.typicode.com/posts').as('getPosts')
+        cy.visit('http://localhost:5173/');
+        cy.wait('@getPosts').its('response.statusCode').should('eq', 200)
+      })
+    
     it("displays the correct title and body of each item returned from API", () => {
       cy.intercept("https://jsonplaceholder.typicode.com/posts").as("fetchPosts");
       cy.wait("@fetchPosts").its("response.statusCode").should("eq", 200);
@@ -16,5 +22,15 @@ describe("MyComponent", () => {
         cy.get("h2").last().should("have.text", "100. at nam consequatur ea labore ea harum");
         cy.get("p").last().contains("cupiditate quo est a modi nesciunt soluta ipsa");
     });
+    
+    it('should not refetch data when re-rendered', () => {
+        cy.intercept('https://jsonplaceholder.typicode.com/posts').as('getPosts')
+        cy.visit('http://localhost:5173/');
+        cy.wait('@getPosts').its('response.statusCode').should('eq', 200)
+        cy.reload()
+        cy.wait('@getPosts', { timeout: 0 }).should(({ request }) => {
+          expect(request).not.to.have.property('response')
+        })
+      })
   
   });

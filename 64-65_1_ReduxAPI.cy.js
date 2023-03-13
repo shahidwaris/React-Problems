5a6aeaf26a2ec94c779b7c7f34cf3d49280e8202
shahidwaris <71723136+shahidwaris@users.noreply.github.com>
2023-03-14 02:53:24 +0530

    Create 64-65_1_ReduxAPI.cy.js

---
+++ 64-65_1_ReduxAPI.cy.js
@@ -0,0 +1,34 @@
+describe('App', () => {
+    beforeEach(() => {
+      cy.visit('http://localhost:5173/');
+    });
+  
+    it('renders the app header', () => {
+      cy.contains('h1', 'User Information');
+    });
+  
+    it('renders the input fields', () => {
+      cy.get('input[type="text"]').should('exist');
+      cy.get('input[type="email"]').should('exist');
+    });
+  
+    it('updates the name field', () => {
+      const name = 'John Doe';
+      cy.get('input[type="text"]').type(name).should('have.value', name);
+    });
+  
+    it('updates the email field', () => {
+      const email = 'john.doe@example.com';
+      cy.get('input[type="email"]').type(email).should('have.value', email);
+    });
+  
+    it('displays the updated values in the output', () => {
+      const name = 'John Doe';
+      const email = 'john.doe@example.com';
+      cy.get('input[type="text"]').type(name);
+      cy.get('input[type="email"]').type(email);
+      cy.get('.output').contains(`Name - ${name}`);
+      cy.get('.output').contains(`Email - ${email}`);
+    });
+  });
+  
